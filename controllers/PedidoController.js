8bd5ccae7fa21925ff5a4b7df2d31061a27d04ad
flip <00653000103@poupex.com.br>
2021-05-04 20:36:38 -0300

    feito crud pedidos

--- controllers/PedidoController.js
+++ controllers/PedidoController.js
@@ -1,26 +1,58 @@
 const Pedido = require('../models/pedidos')
 
-    async function store(req, res) {
-        try{
-            const data = await Pedido.create(req.body)
-            console.log('data no store => ', data)
-            return res.json(data)
-        } catch (e) {
-            console.log('Deu ruim ao salvar pedido. ', e)
-            return res.status(500)
-        }
+async function store(req, res) {
+    try{
+        const data = await Pedido.create(req.body)
+        return res.json(data)
+    } catch (e) {
+        console.log('Deu ruim ao salvar pedido. ', e)
+        return res.status(500)
     }
+}
 
-    async function index(req, res) {
-        try{
-            const data = await Pedido.find({})
-            console.log('data no store => ', data)
-            return res.json(data)
-        } catch (e) {
-            console.log('Deu ruim ao consultar pedidos. ', e)
-            return res.status(500)
-        }
+async function index(req, res) {
+    try{
+        const data = await Pedido.find({})
+        return res.json(data)
+    } catch (e) {
+        console.log('Deu ruim ao consultar pedidos. ', e)
+        return res.status(500)
     }
+}
 
+async function indexOne(req, res) {
+    const { id } = req.params
+    try{
+        const pedido = await Pedido.findById(id)
 
-module.exports = { store, index }
+        if(!pedido){
+            return res.status(404).json({ error: 'Order not found' })
+        }
+
+        return res.json(pedido)
+    } catch (e) {
+        console.log('Deu ruim ao indexar um pedido. ', e)
+        return res.status(500)
+    }
+}
+
+async function erase(req, res) {
+    const { id } = req.params
+
+    try{
+        const pedido = await Pedido.findById(id)
+
+        if(!pedido){
+            return res.status(404).json({ error: "Order not found" })
+        }
+
+        await Pedido.remove({_id: id})
+
+        return res.json({ message: 'Order deleted successfully'})
+    } catch (e) {
+        console.log('Deu ruim ao consultar pedidos. ', e)
+        return res.status(500)
+    }
+}
+
+module.exports = { store, index, indexOne, erase }
--- routes/pedidos.js
+++ routes/pedidos.js
@@ -2,61 +2,17 @@ const express = require('express')
 const router = express.Router()
 const PedidoController = require('../controllers/PedidoController')
 
-let pedidos = [
-    {
-        id: 1,
-        numeroDoPedido: 1,
-        produtos: [
-            {
-                id: 2,
-                qnt: 2,
-                subTotal: 38.50
-            },
-            {
-                id: 3,
-                qnt: 5,
-                subTotal: 9.95
-            },
-        ],
-        clienteNome: 'José Marciano',
-        total: 48.45
-    },
-    {
-        id: 2,
-        numeroDoPedido: 2,
-        produtos: [
-            {
-                id: 1,
-                qnt: 1,
-                subTotal: 25.55
-            },
-            {
-                id: 3,
-                qnt: 2,
-                subTotal: 3.98
-            },
-        ],
-        clienteNome: 'Gretchel M',
-        total: 29.53
-    },
-]
-
-
 /* PEDIDOS */
 // Lista todos os pedidos
 router.get('/', PedidoController.index)
 
 // Lista um pedido por id
-router.get('/:id', (req, res) => {
-
-})
+router.get('/:id', PedidoController.indexOne)
 
 // Cadastra um pedido
 router.post('/', PedidoController.store)
 
 // Deleta um pedido
-router.delete('/:id', (req, res) => {
-
-})
+router.delete('/:id', PedidoController.erase)
 
 module.exports = router
