4dedee3539cf64e93bb48aa2507f2c4e7fda3e97
flip <00653000103@poupex.com.br>
2021-05-07 11:10:33 -0300

    feito models e controller de pedido e iniciado o banco mongo

--- controllers/PedidoController.js
+++ controllers/PedidoController.js
@@ -1,6 +1,20 @@
 const Pedido = require('../models/pedidos')
+const Yup = require('yup')
 
 async function store(req, res) {
+    const schema = Yup.object().shape({
+        clienteNome: Yup.string().required(),
+        produtos: Yup.array(Yup.object().shape({
+            id: Yup.string().required(),
+            qnt: Yup.string().required(),
+            subTotal: Yup.number().required()
+        }))
+    })
+
+    if(!(await schema.isValid(req.body))){
+        return res.status(400).json({ error: 'Validation fails' })
+    }
+
     try{
         const data = await Pedido.create(req.body)
         return res.json(data)
